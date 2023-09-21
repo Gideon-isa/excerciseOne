d11230dfa20e9014c578a005fcabe1099906247f
Gideon Isa <innocentgideon10@gmail.com>
2023-09-21 18:24:20 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("MY name is CSharp");
+
+        int a = 0;
+        if (a == 0) 
+        {
+            Console.WriteLine("Yes they are match");
+        }
     }
 }
